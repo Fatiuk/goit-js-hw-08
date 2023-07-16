109925cccaa61fcc4deefd787418956b29f47775
Fatiuk <125760945+Fatiuk@users.noreply.github.com>
2023-07-16 19:59:24 +0300

    Complete task 3
    Additional fix task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,8 @@
 // Add imports above this line
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
+
 // Change code below this line
 const galleryList = document.querySelector('.gallery');
 const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
@@ -11,51 +12,20 @@ galleryList.insertAdjacentHTML('beforeend', galleryItemsMarkup);
 
 // Create gallery markup from galleryItems (array)
 function createGalleryItemsMarkup(images) {
-    return images
-        .map(({ preview, original, description }) => {
-            return `
+  return images
+    .map(({ preview, original, description }) => {
+      return `
   <li class="gallery__item">
-    <a class="gallery__link" href="${original}">
-      <img
-        class="gallery__image"
-        src="${preview}"
-        data-source="${original}"
-        alt="${description}"
-      />
-    </a>
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
   </li>`;
-        })
-        .join('');
+    })
+    .join('');
 }
 
-// Added image click event
-galleryList.addEventListener('click', selectImage);
-
-function selectImage(e) {
-    if (e.target.nodeName !== 'IMG') return;
-
-    //Disallowed default behavior
-    e.preventDefault();
-
-    //Using the library 'Lightbox' (create image)
-    const instance = basicLightbox.create(
-        `<img src="${e.target.dataset.source}">`,
-        {
-            // EventListener for escapeCloseFunction
-            onShow: instance => {
-                document.addEventListener('keyup', escapeCloseFunction);
-            },
-            onClose: instance => {
-                document.removeEventListener('keyup', escapeCloseFunction);
-            },
-        }
-    );
-
-    // Created function which closes the photo on the 'Escape'
-    function escapeCloseFunction(e) {
-        if (e.key === 'Escape') instance.close();
-    }
-
-    //Using the library 'Lightbox' (open image)
-    instance.show();
-}
+//Using the library 'Simplelightbox' (added options)
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,51 @@
+// Add imports above this line
+import throttle from 'lodash.throttle';
 
+// Work with code below this line
+const formElement = document.querySelector('.feedback-form');
+const FORM_KEY = 'feedback-form-state';
+
+// Save information in localStorage
+function onFormInput(event) {
+  let formData = localStorage.getItem(FORM_KEY);
+  formData = formData ? JSON.parse(formData) : {};
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem(FORM_KEY, JSON.stringify(formData));
+}
+
+// Return information from localStorage to input.value
+function formDataAssembler() {
+  let savedFormData = localStorage.getItem(FORM_KEY);
+
+  if (savedFormData) {
+    savedFormData = JSON.parse(savedFormData);
+    Object.entries(savedFormData).forEach(([key, text]) => {
+      formElement.elements[key].value = text || '';
+    });
+  }
+}
+
+formDataAssembler();
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  //Object Destructuring
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+
+  // Clear inputForm, remove info from localStorage and add empty object
+  if (email.value === '' || message.value === '') {
+    alert('Please provide information for all fields!');
+  } else {
+    let parsedFormData = JSON.parse(localStorage.getItem(FORM_KEY));
+    console.log(parsedFormData);
+
+    event.currentTarget.reset();
+    localStorage.removeItem(FORM_KEY);
+    parsedFormData = {};
+  }
+}
+
+formElement.addEventListener('input', throttle(onFormInput, 500));
+formElement.addEventListener('submit', onFormSubmit);
